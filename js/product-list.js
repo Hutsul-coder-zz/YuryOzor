8964284651dce2f65c0baa4c2182b531a6fea3f8
Igor Marchuk <Jigorua@gmail.com>
2021-11-07 16:40:53 +0200

    added links for products and new bad

--- js/product-list.js
+++ js/product-list.js
@@ -6,21 +6,32 @@
             title: "Стіл",
             description: "Дерев'яний стіл в стилі лофт",
             prise: 4000,
-            image: "img/table.jpg"
+            image: "img/table.jpg",
+            link: "products/wooden-table.html" /* Need add new html pages for this products */
         },
         {
             id: 2,
             title: "Стіл для кухні",
             description: "Дерев'яний стіл для кухні",
             prise: 6000,
-            image: "img/kitchen-table.jpg"
+            image: "img/kitchen-table.jpg",
+            link: "products/kitchen-table.html"
         },
         {
-            id: 2,
+            id: 3,
             title: "Сервант",
             description: "Сервант для посуду",
             prise: 8000,
-            image: "img/servant.jpg"
+            image: "img/servant.jpg",
+            link: "products/servant.html" 
+        },
+        {
+            id: 4,
+            title: "bad",
+            description: "Ліжко",
+            prise: 15000,
+            image: "img/bad.jpg", /* Need add image */
+            link: "products/bad.html"
         }
     ];
 
@@ -33,7 +44,7 @@
                 <h3>${product.title}</h3>
                 <p>${product.description}</p>
                 <div class="buttons">
-                    <a class="button" href="product-info.html">Інформація</a>
+                    <a class="button" href="${product.link}">Інформація</a>
                     <button>${product.prise} грн - Замовити</button>
                 </div>
             </article>
